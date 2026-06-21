08860899fec12b92c130750c032aa122d5c09b1b
Ashish Bhardwaj <149471462+ashish102312@users.noreply.github.com>
2026-06-21 13:40:42 +0530

    Time: 5 ms (68.67%), Space: 43.4 MB (73.31%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i =0; i<n; i++){
+            int paper = n-i;
+            if(citations[i] >= paper){
+                return paper;
+            }
+        }
+        return 0;
+
+    }
+}
