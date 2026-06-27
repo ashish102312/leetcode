c853935d344118b7fed87dc6bac9d1a721aa243a
Ashish Bhardwaj <149471462+ashish102312@users.noreply.github.com>
2026-06-27 22:37:40 +0530

    Time: 141 ms (38.12%), Space: 112.1 MB (75.66%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-      TreeSet<Long> tree = new TreeSet<>();
-      for(int i = 0; i<nums.length; i++){
-        Long candy = tree.ceiling((long) nums[i]-valueDiff);
-        if(candy != null && candy <= (long) nums[i] + valueDiff){
-            return true;
+        TreeSet<Long> tree = new TreeSet<>();
+        for(int i = 0; i<nums.length; i++){
+            Long candy = tree.ceiling((long) nums[i] - valueDiff);
+            if(candy != null && candy <= (long) nums[i] + valueDiff){
+                return true;
+            }
+            tree.add((long)nums[i]);
+            if(i>= indexDiff){
+                tree.remove((long) nums[i - indexDiff]);
+            }
         }
-        tree.add((long) nums[i]);
-        if(i >= indexDiff){
-            tree.remove((long) nums[i - indexDiff]); 
-        }
-      } 
-      return false;
+        return false;
     }
 }
