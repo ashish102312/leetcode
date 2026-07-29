2e734d229c2d75952be9c82ca4f6a02ad3bf4af3
Ashish Bhardwaj <149471462+ashish102312@users.noreply.github.com>
2026-07-29 21:16:49 +0530

    Time: 21 ms (74.19%), Space: 47.1 MB (74.19%) - LeetHub

---
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -0,0 +1,94 @@
+class Solution {
+    static final long LIMIT = 1_000_001L;
+    public String smallestPalindrome(String s, int k) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        int[] half = new int[26];
+        char mid = 0;
+        int len = 0;
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
+            len += half[i];
+            if ((freq[i] & 1) == 1) {
+                mid = (char) ('a' + i);
+            }
+        }
+        if (countWays(half) < k) {
+            return "";
+        }
+        StringBuilder left = new StringBuilder();
+
+        while (len > 0) {
+            for (int c = 0; c < 26; c++) {
+                if (half[c] == 0)
+                    continue;
+                half[c]--;
+                long ways = countWays(half);
+                if (ways >= k) {
+                    left.append((char) ('a' + c));
+                    len--;
+                    break;
+                } else {
+                    k -= ways;
+                    half[c]++;
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder(left);
+        if (mid != 0)
+            ans.append(mid);
+
+        ans.append(new StringBuilder(left).reverse());
+
+        return ans.toString();
+    }
+
+    private long countWays(int[] cnt) {
+
+        int total = 0;
+        for (int x : cnt)
+            total += x;
+
+        long res = 1;
+
+        int remain = total;
+
+        for (int x : cnt) {
+
+            if (x == 0)
+                continue;
+
+            res *= nCr(remain, x);
+
+            if (res > LIMIT)
+                return LIMIT;
+
+            remain -= x;
+        }
+
+        return Math.min(res, LIMIT);
+    }
+
+    private long nCr(int n, int r) {
+
+        if (r > n)
+            return 0;
+
+        r = Math.min(r, n - r);
+
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+
+            ans = ans * (n - r + i) / i;
+
+            if (ans > LIMIT)
+                return LIMIT;
+        }
+
+        return ans;
+    }
+}
