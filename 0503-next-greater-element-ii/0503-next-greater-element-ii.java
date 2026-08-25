3b80361ae3cd1a3def5aa5ac1cf912ba27cf4ecb
Ashish Bhardwaj <149471462+ashish102312@users.noreply.github.com>
2026-08-25 09:35:56 +0530

    Time: 15 ms (60.28%), Space: 47.8 MB (87.5%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -6,8 +6,9 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         for(int i = 2*n-1; i>=0; i--){
             int current = nums[i%n];
-            while(!stack.isEmpty() && stack.peek() <= current)stack.pop();
-
+            while(!stack.isEmpty() && stack.peek() <= current){
+                stack.pop();
+            }
             if(i<n){
                 if(!stack.isEmpty()){
                     ans[i] = stack.peek();
